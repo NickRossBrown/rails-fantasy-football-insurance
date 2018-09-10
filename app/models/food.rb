04fa5ac33431138098d7f980f33abafdf675eb78
Nick Brown <nickrossbrown@gmail.com>
2018-09-10 16:05:07 -0700

    add association between food(belongs_to) and user(has_many)

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,5 @@
 class Food < ActiveRecord::Base
   validates :name, :presence => true
   has_many :reviews
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_225439) do
+ActiveRecord::Schema.define(version: 2018_09_10_230235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2018_09_10_225439) do
     t.boolean "featured"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
