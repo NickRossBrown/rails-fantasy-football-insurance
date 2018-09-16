a0530da6ca480a52e9b2e674786bc9b2dd8074b7
Nick Brown <nickrossbrown@gmail.com>
2018-09-16 17:08:51 -0600

    add two passing test. One for User and one for Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ActiveRecord::Base
   validates :description, :presence => true
-  # validates :user_rating, :presence => true
+  validates :user, :presence => true
   belongs_to :food
 
   scope :five_most_recent, -> { order(created_at: :desc).limit(5)}
 
-  
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
  describe Review do
   it { should validate_presence_of :description }
-  it { should validate_presence_of :user_rating }
+  it { should validate_presence_of :user }
   it { should belong_to :food }
 end
