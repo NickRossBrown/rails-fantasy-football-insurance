6ad768515d513c5063ad5c4fee4ac7f34da88d7b
Nick Brown <nickrossbrown@gmail.com>
2018-09-16 11:04:35 -0600

    only display the five most recent reviews in the food show page

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,8 +4,6 @@ class Food < ActiveRecord::Base
   belongs_to :user
   has_one_attached :image
 
-  scope :five_most_recent, -> { order(created_at: :desc).limit(5)}
-
   scope :most_reviews, -> {(
     select("foods.id, foods.name, foods.description, count(reviews.id) as reviews_count")
     .joins(:reviews)
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ActiveRecord::Base
   validates :description, :presence => true
   # validates :user_rating, :presence => true
   belongs_to :food
+
+  scope :five_most_recent, -> { order(created_at: :desc).limit(5)}
+
+  
 end
